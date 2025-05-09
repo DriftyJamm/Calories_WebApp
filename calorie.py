4ef9ffe4cdb36095b75d4248bd786e84c45cd114
DriftyJamm <92976385+DriftyJamm@users.noreply.github.com>
2025-05-09 20:41:38 +0530

    updated

---
+++ calorie.py
@@ -0,0 +1,9 @@
+class Calorie:
+    def __init__(self, weight, height, age, temperature):
+        self.temperature = temperature
+        self.weight = weight
+        self.height = height
+        self.age = age
+
+    def calculate(self):
+        pass
